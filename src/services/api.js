c907f7a305a96c609d6464c4fd0d9c38dc0ae09d
Michał Madejski <49588850+yx-aesthete@users.noreply.github.com>
2026-04-07 23:23:11 +0200

    fix: use REACT_APP_API_URL env var

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = "https://graph-vis-be.onrender.com";
-// const API_URL = "http://192.168.100.98:5001/";
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5001";
 
 export const getBfsTraversal = async (graph) => {
   const response = await axios.post(`${API_URL}/traversal/bfs`, graph);
@@ -35,21 +33,6 @@ export const generateRandomBuildGraph = async (
     num_edges: numEdges,
     connectivity: connectivity,
     additional_params: additionalParams,
-  });
-  return response.data;
-};
-
-export const generateTraversalSequence = async (
-  graph,
-  startNode,
-  algorithm,
-  goalNode = null
-) => {
-  const response = await axios.post(`${API_URL}/traversal`, {
-    graph: graph,
-    start_node: startNode,
-    algorithm: algorithm,
-    goal_node: goalNode,
   });
   return response.data;
 };
